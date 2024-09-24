d062e449f097564bd4e47ef9eed1c245f1ac59eb
Melvin <kirigayakazzeno@gmail.com>
2024-09-24 19:28:01 +0700

    RAiso

---
+++ RAiso/View/StationeryDetails.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout/Navbar.Master" AutoEventWireup="true" CodeBehind="StationeryDetails.aspx.cs" Inherits="RAiso.View.StationeryDetails" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="../Styles/StationeryDetails.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="CenterBox">
+        <div class="container1180px">
+            <div class="imgcontainer">
+                <img src="../Assets/StationeryHome.jpg" alt="">
+            </div>
+            <div class="flexcolumn detailcontainer">
+                <asp:Label ID="Name" CssClass="StatName" runat="server" Text="Label"></asp:Label>
+                <asp:Label ID="Price" CssClass="StatPrice" runat="server" Text="Rp.10000"></asp:Label>
+                <asp:Label ID="Description" CssClass="wrap-text Descriptions" runat="server" Text="RAiso is your premier destination for top-quality stationery, offering a vast selection to meet all your needs. Whether you're a student, professional, or creative enthusiast, RAiso provides carefully selected products that combine functionality with style, from elegant pens and pencils to stylish notebooks and planners."></asp:Label>
+
+                <asp:Panel ID="BtnContainer" runat="server" CssClass="flexcolumn btncontainer">
+                    <asp:Label ID="Utility1" CssClass="Utility1" Visible="false" runat="server" Text="Specify Quantity"></asp:Label>
+                    <asp:TextBox ID="Quantity" CssClass="QuantityBox" Visible="false" runat="server" Text="1" TextMode="Number"></asp:TextBox>
+                    <asp:Button ID="AddCartBtn" CssClass="cardbtn1" Visible="false" runat="server" Text="Add to Cart" OnClick="AddCartBtn_Click" />
+                    <asp:Button ID="PurchaseBtn" CssClass="cardbtn2" Visible="false" runat="server" Text="Purchase" OnClick="PurchaseBtn_Click" />
+                    <asp:Label ID="lblAlert" runat="server" Visible="false" Text=""></asp:Label>
+                </asp:Panel>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ RAiso/View/StationeryDetails.aspx.cs
@@ -0,0 +1,119 @@
+﻿using RAiso.Controller;
+using RAiso.Models;
+using RAiso.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RAiso.View
+{
+    public partial class StationeryDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["User"] == null && Request.Cookies["UserCookie"] != null)
+            {
+                String cookie = Request.Cookies["UserCookie"].Value;
+                Response<MsUser> response = MsUserController.LogInWithCookie(cookie);
+                if (response.IsSuccess == false)
+                {
+                    Response.Cookies["UserCookie"].Expires = DateTime.Now.AddDays(-1);
+                }
+                Session["User"] = response.Payload;
+            }
+            if (!IsPostBack)
+            {
+                string stationeryID = Request.QueryString["stationeryID"];
+                if (!string.IsNullOrEmpty(stationeryID))
+                {
+                    if (decimal.TryParse(stationeryID, out decimal IDTemp))
+                    {
+                        int ID = (int)Math.Round(IDTemp);
+                        StatDetail(ID);
+                    }
+                }
+                else // No StationeryID
+                {
+                    Response.Redirect("~/View/Home.aspx");
+                }
+            }
+
+            MsUser user = Session["User"] as MsUser;
+            if (user != null && user.UserRole.ToLower() == "admin")
+            {
+                AddCartBtn.Visible = false;
+                PurchaseBtn.Visible = false;
+                Quantity.Visible = false;
+                Utility1.Visible = false;
+                BtnContainer.Visible = false;
+            }
+            else if (user != null && user.UserRole.ToLower() == "user")
+            {
+                AddCartBtn.Visible = true;
+                PurchaseBtn.Visible = true;
+                Quantity.Visible = true;
+                Utility1.Visible = true;
+                BtnContainer.Visible = true;
+            }
+            else
+            {
+                AddCartBtn.Visible = false;
+                PurchaseBtn.Visible = false;
+                Quantity.Visible = false;
+                Utility1.Visible = false;
+                BtnContainer.Visible = false;
+            }
+        }
+
+        public void StatDetail(int ID)
+        {
+            Response<MsStationery> response = MsStationeryController.GetStationeryById(ID);
+            if (response.IsSuccess)
+            {
+                MsStationery stationery = response.Payload;
+                Name.Text = stationery.StationeryName;
+                Price.Text = stationery.StationeryPrice.ToString();
+            }
+        }
+
+        protected void AddCartBtn_Click(object sender, EventArgs e)
+        {
+
+            MsUser user = Session["User"] as MsUser;
+            int userId = user.UserID;
+            int statId = -1;
+            int qty = Convert.ToInt32(Quantity.Text);
+
+            string stationeryID = Request.QueryString["stationeryID"];
+            if (!string.IsNullOrEmpty(stationeryID))
+            {
+                if (decimal.TryParse(stationeryID, out decimal IDTemp))
+                {
+                    statId = (int)Math.Round(IDTemp);
+                }
+            }
+            Response<RAiso.Models.Cart> cart = CartController.AddToCart(userId, statId, qty);
+            
+            if(cart.IsSuccess == false)
+            {
+                lblAlert.ForeColor = System.Drawing.Color.Red;
+                lblAlert.Text = cart.Message;
+                lblAlert.Visible = true;
+            }
+            else
+            {
+                lblAlert.ForeColor = System.Drawing.Color.Green;
+                lblAlert.Text = cart.Message;
+                lblAlert.Visible = true;
+            }
+        }
+
+        protected void PurchaseBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
